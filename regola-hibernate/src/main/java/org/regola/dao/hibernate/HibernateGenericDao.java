c7f981291eb5c9a329a6d2cdda94a03e76d1fbbe
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-08-26 15:18:08 +0000

    Aggiunta implementazione Hibernate di GenericDao e Criteria

---
+++ regola-hibernate/src/main/java/org/regola/dao/hibernate/HibernateGenericDao.java
@@ -0,0 +1,87 @@
+package org.regola.dao.hibernate;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.regola.DefaultFilterBuilder;
+import org.regola.FilterBuilder;
+import org.regola.GenericDao;
+import org.regola.ModelFilter;
+import org.regola.filter.criteria.hibernate.HibernateCriteria;
+import org.springframework.orm.ObjectRetrievalFailureException;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class HibernateGenericDao<T, ID extends Serializable> extends
+		HibernateDaoSupport implements GenericDao<T, ID> {
+
+	private Class<T> persistentClass;
+
+	private FilterBuilder filterBuilder = new DefaultFilterBuilder();
+
+	@SuppressWarnings("unchecked")
+	public ID create(T entity) {
+		return (ID) getHibernateTemplate().save(entity);
+	}
+
+	public T read(ID id) {
+		@SuppressWarnings("unchecked")
+		T entity = (T) getHibernateTemplate().get(persistentClass, id);
+
+		if (entity == null) {
+			throw new ObjectRetrievalFailureException(persistentClass, id);
+		}
+
+		return entity;
+	}
+
+	public void update(T entity) {
+		getHibernateTemplate().update(entity);
+	}
+
+	public void delete(T entity) {
+		getHibernateTemplate().delete(entity);
+	}
+
+	public void save(T entity) {
+		getHibernateTemplate().saveOrUpdate(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> find(final ModelFilter filter) {
+		return (List<T>) getHibernateTemplate().executeFind(
+				new HibernateCallback() {
+					public Object doInHibernate(Session session) {
+						HibernateCriteria criteriaBuilder = new HibernateCriteria(
+								session.createCriteria(persistentClass));
+						getFilterBuilder()
+								.createFilter(criteriaBuilder, filter);
+						return criteriaBuilder.getCriteria().list();
+					}
+				});
+	}
+
+	public int count(final ModelFilter filter) {
+		// TODO controllare null result
+		return (Integer) getHibernateTemplate().execute(
+				new HibernateCallback() {
+					public Object doInHibernate(Session session) {
+						HibernateCriteria criteriaBuilder = new HibernateCriteria(
+								session.createCriteria(persistentClass));
+						getFilterBuilder().createCountFilter(criteriaBuilder,
+								filter);
+						return criteriaBuilder.getCriteria().uniqueResult();
+					}
+				});
+	}
+
+	public FilterBuilder getFilterBuilder() {
+		return filterBuilder;
+	}
+
+	public void setFilterBuilder(FilterBuilder filterBuilder) {
+		this.filterBuilder = filterBuilder;
+	}
+
+}
---
+++ regola-hibernate/src/main/java/org/regola/filter/criteria/hibernate/HibernateCriteria.java
@@ -0,0 +1,97 @@
+package org.regola.filter.criteria.hibernate;
+
+import java.util.Collection;
+
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.regola.AbstractCriteriaBuilder;
+import org.regola.Criteria;
+import org.regola.criterion.Order;
+
+public class HibernateCriteria extends AbstractCriteriaBuilder {
+
+	private org.hibernate.Criteria criteria;
+
+	public HibernateCriteria(org.hibernate.Criteria criteria) {
+		this.criteria = criteria;
+	}
+
+	@Override
+	public void addEquals(String property, Object value) {
+		criteria.add(Restrictions.eq(property, value));
+	}
+
+	@Override
+	public void addNotEquals(String property, Object value) {
+		criteria.add(Restrictions.ne(property, value));
+	}
+
+	@Override
+	public void addGreaterThan(String property, Object value) {
+		criteria.add(Restrictions.gt(property, value));
+	}
+
+	@Override
+	public void addLessThan(String property, Object value) {
+		criteria.add(Restrictions.lt(property, value));
+	}
+
+	@Override
+	public void addLike(String property, String value) {
+		criteria.add(Restrictions.like(property, value));
+	}
+
+	@Override
+	public void addIlike(String property, String value) {
+		criteria.add(Restrictions.ilike(property, value));
+	}
+
+	@Override
+	public void addGreaterEquals(String property, Object value) {
+		criteria.add(Restrictions.ge(property, value));
+	}
+
+	@Override
+	public void addLessEquals(String property, Object value) {
+		criteria.add(Restrictions.le(property, value));
+	}
+
+	@Override
+	public void addIn(String property, Collection<?> value) {
+		criteria.add(Restrictions.in(property, value));
+	}
+
+	@Override
+	public Criteria addOrder(Order order) {
+		if (order.isAscending()) {
+			criteria.addOrder(org.hibernate.criterion.Order.asc(order
+					.getPropertyName()));
+		} else {
+			criteria.addOrder(org.hibernate.criterion.Order.desc(order
+					.getPropertyName()));
+		}
+		return this;
+	}
+
+	@Override
+	public Criteria setFirstResult(int firstResult) {
+		criteria.setFirstResult(firstResult);
+		return this;
+	}
+
+	@Override
+	public Criteria setMaxResults(int maxResults) {
+		criteria.setMaxResults(maxResults);
+		return this;
+	}
+
+	@Override
+	public void setRowCount() {
+		criteria.setProjection(Projections.rowCount());
+	}
+
+	public org.hibernate.Criteria getCriteria() {
+		return criteria;
+	}
+
+}
