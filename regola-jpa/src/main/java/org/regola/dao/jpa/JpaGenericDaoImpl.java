c614d1ac87ac7f44eae386e252511a995f8ca6ff
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-08-31 16:05:42 +0000

    Aggiunta implementazione JPA del GenericDao

---
+++ regola-jpa/src/main/java/org/regola/dao/jpa/JpaGenericDaoImpl.java
@@ -0,0 +1,160 @@
+package org.regola.dao.jpa;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.EntityManager;
+import javax.persistence.Id;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.regola.dao.GenericDao;
+import org.regola.filter.FilterBuilder;
+import org.regola.filter.ModelFilter;
+import org.regola.filter.builder.DefaultFilterBuilder;
+import org.regola.filter.criteria.jpa.JpaCriteria;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.orm.ObjectRetrievalFailureException;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JpaGenericDaoImpl<T, ID extends Serializable> implements
+		GenericDao<T, ID> {
+
+	private EntityManager entityManager;
+
+	private Class<T> persistentClass;
+
+	private FilterBuilder filterBuilder = new DefaultFilterBuilder();
+
+	private Method idGetter;
+
+	@PersistenceContext
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public JpaGenericDaoImpl(Class<T> clazz) {
+		this.persistentClass = clazz;
+		initId();
+	}
+
+	protected void initId() {
+		for (Method method : persistentClass.getMethods()) {
+			if (method.isAnnotationPresent(Id.class)
+					|| method.isAnnotationPresent(EmbeddedId.class)) {
+				idGetter = method;
+				return;
+			}
+		}
+		for (Field field : persistentClass.getFields()) {
+			if (field.isAnnotationPresent(Id.class)
+					|| field.isAnnotationPresent(EmbeddedId.class)) {
+				try {
+					idGetter = persistentClass
+							.getMethod(getPropertyGetter(field.getName()));
+				} catch (SecurityException e) {
+					throw new InvalidDataAccessApiUsageException(
+							"Could not find persistent Id accessor", e);
+				} catch (NoSuchMethodException e) {
+					throw new InvalidDataAccessApiUsageException(
+							"Could not find persistent Id accessor", e);
+				}
+			}
+		}
+	}
+
+	protected String getPropertyGetter(String versionProperty) {
+		return "get" + Character.toUpperCase(versionProperty.charAt(0))
+				+ versionProperty.substring(1);
+	}
+
+	@SuppressWarnings("unchecked")
+	public ID create(T object) {
+		entityManager.persist(object);
+		try {
+			return (ID) idGetter.invoke(object);
+		} catch (IllegalArgumentException e) {
+			throw new InvalidDataAccessApiUsageException(
+					"Error getting persistent Id", e);
+		} catch (IllegalAccessException e) {
+			throw new InvalidDataAccessApiUsageException(
+					"Error getting persistent Id", e);
+		} catch (InvocationTargetException e) {
+			throw new InvalidDataAccessApiUsageException(
+					"Error getting persistent Id", e);
+		}
+	}
+
+	public T read(ID id) {
+		T object = entityManager.find(persistentClass, id);
+
+		if (object == null) {
+			throw new ObjectRetrievalFailureException(persistentClass, id);
+		}
+
+		return object;
+	}
+
+	public void update(T object) {
+		entityManager.merge(object);
+	}
+
+	public void delete(T object) {
+		entityManager.remove(object);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> find(String finder, Object... args) {
+		Query query = entityManager.createNamedQuery(getQueryName(finder));
+		for (int i = 0; i < args.length; i++) {
+			query.setParameter(i, args[i]);
+		}
+		return (List<T>) query.getResultList();
+	}
+
+	protected String getQueryName(String query) {
+		return persistentClass.getSimpleName() + "." + query;
+	}
+
+	public int count(ModelFilter filter) {
+		JpaCriteria criteriaBuilder = new JpaCriteria(persistentClass,
+				entityManager);
+		getFilterBuilder().createCountFilter(criteriaBuilder, filter);
+		return (Integer) criteriaBuilder.getQuery().getSingleResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> find(ModelFilter filter) {
+		JpaCriteria criteriaBuilder = new JpaCriteria(persistentClass,
+				entityManager);
+		getFilterBuilder().createFilter(criteriaBuilder, filter);
+		return criteriaBuilder.getQuery().getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> getAll() {
+		JpaCriteria criteriaBuilder = new JpaCriteria(persistentClass,
+				entityManager);
+		getFilterBuilder().createFilter(criteriaBuilder, new ModelFilter() {
+		});
+		return criteriaBuilder.getQuery().getResultList();
+	}
+
+	public void save(T object) {
+		entityManager.persist(object);
+	}
+
+	public FilterBuilder getFilterBuilder() {
+		return filterBuilder;
+	}
+
+	public void setFilterBuilder(FilterBuilder filterBuilder) {
+		this.filterBuilder = filterBuilder;
+	}
+
+}
---
+++ regola-jpa/src/test/java/org/regola/dao/jpa/JpaGenericDaoTest.java
@@ -0,0 +1,10 @@
+package org.regola.dao.jpa;
+
+import org.junit.runner.RunWith;
+import org.regola.dao.BaseGenericDaoTest;
+import org.unitils.UnitilsJUnit4TestClassRunner;
+
+@RunWith(UnitilsJUnit4TestClassRunner.class)
+public class JpaGenericDaoTest extends BaseGenericDaoTest {
+
+}
