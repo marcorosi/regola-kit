39d56469eaa29eb3724730fe7474708c4f0e48a5
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-08-31 12:39:55 +0000

    Aggiunto test per implementazione Hibernate del GenericDao

---
+++ trunk/regola-hibernate/src/test/java/org/regola/dao/hibernate/HibernateGenericDaoTest.java
@@ -0,0 +1,10 @@
+package org.regola.dao.hibernate;
+
+import org.junit.runner.RunWith;
+import org.regola.dao.BaseGenericDaoTest;
+import org.unitils.UnitilsJUnit4TestClassRunner;
+
+@RunWith(UnitilsJUnit4TestClassRunner.class)
+public class HibernateGenericDaoTest extends BaseGenericDaoTest {
+
+}
