87531d720beb28861ea3da66bbbddc5d5113911c
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-09-10 23:55:29 +0000

    Corretto test per HibernateGenericDao

--- regola-hibernate/src/test/java/org/regola/dao/hibernate/HibernateGenericDaoTest.java
+++ regola-hibernate/src/test/java/org/regola/dao/hibernate/HibernateGenericDaoTest.java
@@ -2,9 +2,17 @@ package org.regola.dao.hibernate;
 
 import org.junit.runner.RunWith;
 import org.regola.dao.BaseGenericDaoTest;
+import org.springframework.orm.hibernate3.HibernateAccessor;
 import org.unitils.UnitilsJUnit4TestClassRunner;
+import org.unitils.spring.annotation.SpringBeanByName;
 
 @RunWith(UnitilsJUnit4TestClassRunner.class)
 public class HibernateGenericDaoTest extends BaseGenericDaoTest {
+
+	@SpringBeanByName
+	public static void setCustomerDao(HibernateGenericDao<?, ?> customerDao) {
+		customerDao.getHibernateTemplate().setFlushMode(
+				HibernateAccessor.FLUSH_EAGER);
+	}
 
 }
