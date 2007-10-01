cf6543f2645a73ee5fa931d92adc398c0508bf46
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-10-01 18:36:29 +0000

    Completed GenericDao tests regarding ModelPattern annotations (issue #7, partial)

--- regola-core/src/test/java/org/regola/model/CustomerPattern.java
+++ regola-core/src/test/java/org/regola/model/CustomerPattern.java
@@ -1,11 +1,36 @@
 package org.regola.model;
 
 import org.regola.filter.annotation.Equals;
+import org.regola.filter.annotation.GreaterThan;
 import org.regola.filter.annotation.In;
+import org.regola.filter.annotation.LessThan;
 import org.regola.filter.annotation.Like;
+import org.regola.filter.annotation.NotEquals;
 
 public class CustomerPattern extends ModelPattern {
 
+	@GreaterThan
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@LessThan("id")
+	private Integer lessThanId;
+
+	public Integer getLessThanId() {
+		return lessThanId;
+	}
+
+	public void setLessThanId(Integer id) {
+		this.lessThanId = id;
+	}
+	
 	@Equals
 	private String firstName;
 
@@ -15,6 +40,17 @@ public class CustomerPattern extends ModelPattern {
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+
+	@NotEquals("firstName")
+	private String notEqualsFirstName;
+
+	public String getNotEqualsFirstName() {
+		return notEqualsFirstName;
+	}
+
+	public void setNotEqualsFirstName(String firstName) {
+		this.notEqualsFirstName = firstName;
 	}
 
 	@In("lastName")
