9f295d1763249096f19e17816a6fc150473ca627
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-10-19 23:38:07 +0000

    [core, ibatis, hibernate, jpa] Disabled some FinderExecutor tests (issues 6, 7, incomplete)

--- trunk/regola-core/src/test/java/org/regola/dao/AbstractFinderDaoTest.java
+++ trunk/regola-core/src/test/java/org/regola/dao/AbstractFinderDaoTest.java
@@ -5,10 +5,32 @@ import java.util.List;
 
 import org.regola.model.Customer;
 import org.regola.model.Customer.Address;
+import org.springframework.test.annotation.IfProfileValue;
+import org.springframework.test.annotation.ProfileValueSource;
+import org.springframework.test.annotation.SystemProfileValueSource;
 
 public abstract class AbstractFinderDaoTest extends AbstractGenericDaoTest {
 
 	private CustomerDao customerFinderDao;
+
+	public class DefaultEnabledSystemProfileValueSource implements
+			ProfileValueSource {
+
+		private ProfileValueSource delegate;
+
+		public DefaultEnabledSystemProfileValueSource() {
+			this.delegate = SystemProfileValueSource.getInstance();
+		}
+
+		public String get(String key) {
+			String profileValue = delegate.get(key);
+			return profileValue == null ? "enabled" : profileValue;
+		}
+	}
+
+	public AbstractFinderDaoTest() {
+		profileValueSource = new DefaultEnabledSystemProfileValueSource();
+	}
 
 	public CustomerDao getCustomerFinderDao() {
 		return customerFinderDao;
@@ -31,6 +53,7 @@ public abstract class AbstractFinderDaoTest extends AbstractGenericDaoTest {
 		assertTrue(customersId.contains(47));
 	}
 
+	@IfProfileValue(name = "testExecuteFinder_byAddressCity", value = "enabled")
 	public void testExecuteFinder_byAddressCity() {
 		Address address = newAddress("unused", "Lyon");
 		List<Customer> customers = customerFinderDao.findByAddressCity(address);
@@ -47,6 +70,7 @@ public abstract class AbstractFinderDaoTest extends AbstractGenericDaoTest {
 		assertTrue(customersId.contains(38));
 	}
 
+	@IfProfileValue(name = "testExecuteFinder_byAddress", value = "enabled")
 	public void testExecuteFinder_byAddress() {
 		Address address = newAddress("33 Upland Pl.", "Seattle");
 		List<Customer> customers = customerFinderDao.findByAddress(address);
