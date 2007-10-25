329ac9bf888174bace9a1e9b378d6d78c414fa2b
nicolasanti <nicolasanti@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-10-25 10:03:23 +0000

    piccole perfezionamenti

--- regola-kit/regola-example-simple/src/main/java/org/regola/webapp/action/CustomerList.java
+++ regola-kit/regola-example-simple/src/main/java/org/regola/webapp/action/CustomerList.java
@@ -15,32 +15,11 @@ public class CustomerList extends ListPage<Customer, Integer, CustomerPattern>
 	public void init()
 	{
 		setFilter(new CustomerPattern());
-		//@TODO Imposta un criterio per il figlio, ad esempio
-		//getFilter().setAnnoAccademico(2005);
-
+		//@TODO Imposta un criterio per il filtro, ad esempio
+		
 		getEventBroker().subscribe(this, "customer.persistence.changes");
 		
 		super.init();
-	}
-	
-	/**
-	 * Questo metodo è chiamato per aggiornare il modello
-	 * da presentare a video ad esempio dopo una modifica al filtro
-	 */
-	@Override
-	public void refresh()
-	{
-		super.refresh();
-	}
-	
-	/**
-	 * Salta alla pagina di editing
-	 */
-	public String edit()
-	{
-		Customer v = getCurrentModelItem();
-		log.info("Selezionato "+v);
-		return "edit";
 	}
 	
 	/**
