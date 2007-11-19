27e3bacd3723bba393f4138f57978af9676ca153
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-11-19 23:08:38 +0000

    Working on issue 21: filtraggio di entità associate

--- trunk/regola-kit/regola-dao/regola-hibernate/src/main/java/org/regola/filter/criteria/hibernate/HibernateCriteria.java
+++ trunk/regola-kit/regola-dao/regola-hibernate/src/main/java/org/regola/filter/criteria/hibernate/HibernateCriteria.java
@@ -3,141 +3,158 @@ package org.regola.filter.criteria.hibernate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.regola.filter.criteria.Criteria;
 import org.regola.filter.criteria.Order;
 import org.regola.filter.criteria.impl.AbstractQueryBuilder;
 
-/**
- * @author nicola
- */
 public class HibernateCriteria extends AbstractQueryBuilder {
 
-    protected final Log log = LogFactory.getLog(getClass());
-        
-    private static final String ROOT = "CriteriaPseudoBuilder_ROOT";
-	private Map<String, org.hibernate.Criteria> criteri;
-	//private org.hibernate.Criteria criteria;
+	protected final Log log = LogFactory.getLog(getClass());
+
+	private static final String ROOT = "CriteriaPseudoBuilder_ROOT";
+	private Map<String, org.hibernate.Criteria> criteriaMap;
+
+	class CriteriaBuilder {
+		String propertyPath;
+		org.hibernate.Criteria criteria;
+
+		public CriteriaBuilder(String propertyPath,
+				org.hibernate.Criteria criteria) {
+			this.propertyPath = propertyPath;
+			this.criteria = criteria;
+		}
+
+		public void eq(Object value) {
+			criteria.add(Restrictions.eq(propertyPath, value));
+		}
+
+		public void ne(Object value) {
+			criteria.add(Restrictions.ne(propertyPath, value));
+		}
+
+		public void gt(Object value) {
+			criteria.add(Restrictions.gt(propertyPath, value));
+		}
+
+		public void lt(Object value) {
+			criteria.add(Restrictions.lt(propertyPath, value));
+		}
+
+		public void like(Object value) {
+			criteria.add(Restrictions.like(propertyPath, value));
+		}
+
+		public void ilike(Object value) {
+			criteria.add(Restrictions.ilike(propertyPath, value));
+		}
+
+		public void ge(Object value) {
+			criteria.add(Restrictions.ge(propertyPath, value));
+		}
+
+		public void le(Object value) {
+			criteria.add(Restrictions.le(propertyPath, value));
+		}
+
+		public void in(Collection<?> value) {
+			criteria.add(Restrictions.in(propertyPath, value));
+		}
+	}
 
 	public HibernateCriteria(org.hibernate.Criteria criteria) {
-            	criteri = new HashMap<String, org.hibernate.Criteria>();
-		criteri.put(ROOT, criteria);
-		//this.criteria = criteria;
+		criteriaMap = new HashMap<String, org.hibernate.Criteria>();
+		criteriaMap.put(ROOT, criteria);
 	}
 
-    private org.hibernate.Criteria getCriteria(String propertyPath)
-	{ 
-		if(propertyPath == null)
+	private CriteriaBuilder getCriteria(String propertyPath) {
+		if (propertyPath == null) {
 			throw new RuntimeException("propertyPath non può essere null");
-		
-		if(propertyPath.indexOf(".") < 0 || propertyPath.startsWith("id."))
-			return getRootCriteria();
+		}
 
-		String path = propertyPath.substring(0,propertyPath.lastIndexOf(".")); 
-		
-		if(!criteri.containsKey(path))
-		{
-			creaCriterio(path, getRootCriteria());				
-		}
-		
-		return criteri.get(path);
-	}
-        
-    private void creaCriterio(String path, org.hibernate.Criteria criteria) 
-	{
-		if(path.indexOf(".") < 0)
-		{
-			criteri.put(path, criteria.createCriteria(path));
-			log.info("createCriteria("+path+")");
-		}
-		else
-		{
-			String subPath = path.substring(0,path.indexOf("."));
-			if(!criteri.containsKey(subPath))
-			{
-				creaCriterio(subPath, criteria);
+		StringBuilder fullPath = new StringBuilder();
+		org.hibernate.Criteria criteria = getRootCriteria();
+		if (propertyPath.contains("[]")) {
+			String[] paths = propertyPath.split("\\[\\]\\.?");
+			for (int i = 0; i < paths.length; i++) {
+				propertyPath = paths[i];
+				if (i < paths.length - 1) {
+					fullPath.append(paths[i]);
+					if (criteriaMap.containsKey(fullPath)) {
+						criteria = criteriaMap.get(fullPath);
+					} else {
+						criteria = criteria.createCriteria(paths[i]);
+						criteriaMap.put(fullPath.toString(), criteria);
+					}
+					fullPath.append("[]");
+				}
 			}
-			String alias = path.substring(path.lastIndexOf(".")+1,path.length());
-			criteri.put(path, criteri.get(subPath).createCriteria(alias));
-			log.info("createCriteria("+alias+")");
 		}
-	}
-        
-    private void add(String property, Criterion exp) {
-        getCriteria(property).add(exp);
-    }
-
-	private String getPropertyName(String propertyPath) 
-	{
-		if(propertyPath == null)
-			throw new RuntimeException("propertyPath non può essere null");
 		
-		if(propertyPath.lastIndexOf(".") >= 0 && !propertyPath.startsWith("id."))
-		{
-			return propertyPath.substring(propertyPath.lastIndexOf(".")+1,propertyPath.length());
-		} else
-		{
-			return propertyPath;
-		}
+		log.debug("getCriteria(): path=" + propertyPath + ", fullPath="
+				+ fullPath);
+		return new CriteriaBuilder(propertyPath, criteria);
 	}
-    	
+
 	@Override
 	public void addEquals(String property, Object value) {
-		add(property, Restrictions.eq(getPropertyName(property), value));
+		getCriteria(property).eq(value);
 	}
 
 	@Override
 	public void addNotEquals(String property, Object value) {
-		add(property, Restrictions.ne(property, value));
+		getCriteria(property).ne(value);
 	}
 
 	@Override
 	public void addGreaterThan(String property, Object value) {
-		add(property, Restrictions.gt(property, value));
+		getCriteria(property).gt(value);
 	}
 
 	@Override
 	public void addLessThan(String property, Object value) {
-		add(property, Restrictions.lt(property, value));
+		getCriteria(property).lt(value);
 	}
 
 	@Override
 	public void addLike(String property, String value) {
-		add(property, Restrictions.like(property, value + "%"));
+		getCriteria(property).like(value + "%");
 	}
 
 	@Override
 	public void addIlike(String property, String value) {
-		add(property, Restrictions.ilike(property, value + "%"));
+		getCriteria(property).ilike(value + "%");
 	}
 
 	@Override
 	public void addGreaterEquals(String property, Object value) {
-		add(property, Restrictions.ge(property, value));
+		getCriteria(property).ge(value);
 	}
 
 	@Override
 	public void addLessEquals(String property, Object value) {
-		add(property, Restrictions.le(property, value));
+		getCriteria(property).le(value);
 	}
 
 	@Override
 	public void addIn(String property, Collection<?> value) {
-		add(property, Restrictions.in(property, value));
+		getCriteria(property).in(value);
 	}
 
 	@Override
 	public Criteria addOrder(Order order) {
 		if (order.isAscending()) {
-			getRootCriteria().addOrder(org.hibernate.criterion.Order.asc(order
-					.getPropertyName()));
+			getRootCriteria().addOrder(
+					org.hibernate.criterion.Order.asc(order.getPropertyName()));
 		} else {
-			getRootCriteria().addOrder(org.hibernate.criterion.Order.desc(order
-					.getPropertyName()));
+			getRootCriteria()
+					.addOrder(
+							org.hibernate.criterion.Order.desc(order
+									.getPropertyName()));
 		}
 		return this;
 	}
@@ -160,12 +177,12 @@ public class HibernateCriteria extends AbstractQueryBuilder {
 	}
 
 	public org.hibernate.Criteria getCriteria() {
-		//return criteria;
-                return getRootCriteria();
+		// return criteria;
+		return getRootCriteria();
 	}
 
-    private org.hibernate.Criteria getRootCriteria() {
-            return criteri.get(ROOT);
-    }
+	private org.hibernate.Criteria getRootCriteria() {
+		return criteriaMap.get(ROOT);
+	}
 
 }
