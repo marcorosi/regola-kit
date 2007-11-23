8f4d590b0445eab430565d48173985e212045b5b
nicolasanti <nicolasanti@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-11-23 15:19:32 +0000

    modulo per un altro toolkit di componenti jsf: le Web Galileo Faces

---
+++ trunk/regola-kit/regola-jsf-components/regola-jsf-webgalileo/src/main/java/org/regola/webapp/jsf/webgalileo/DialogWebGalileo.java
@@ -0,0 +1,20 @@
+package org.regola.webapp.jsf.webgalileo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+
+
+public class DialogWebGalileo {
+	
+	protected final Log log = LogFactory.getLog(getClass());
+	
+	public String updateStatus()
+	{
+		return null;
+	}
+
+	
+  }
+
