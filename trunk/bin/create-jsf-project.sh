34c6bb03738b5754fcec5e93cbfb643871c07cc8
nicolasanti <nicolasanti@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-11-28 14:08:26 +0000

    adeguato alla versione 1.0-M1 dell'archetipo

--- trunk/bin/create-jsf-project.sh
+++ trunk/bin/create-jsf-project.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn archetype:create -DarchetypeGroupId=org.regola  -DarchetypeArtifactId=regola-jsf-archetype -DarchetypeVersion=1.0-M1-SNAPSHOT -DgroupId=$1 -DartifactId=$2 
+mvn archetype:create -DarchetypeGroupId=org.regola  -DarchetypeArtifactId=regola-jsf-archetype -DarchetypeVersion=1.0-M1 -DgroupId=$1 -DartifactId=$2 
