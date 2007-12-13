e67a059e76de3919a9496f1f277848f9deb61632
marco.rosi <marco.rosi@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2007-12-13 08:17:23 +0000

    fix

--- regola-kit/regola-codeassistence/src/main/java/org/regola/codeassistence/Utils.java
+++ regola-kit/regola-codeassistence/src/main/java/org/regola/codeassistence/Utils.java
@@ -10,15 +10,15 @@ public class Utils {
 	public static String lowerFirstLetter(String s)
 	{
 		if(s != null && s.length() > 0)
-			s = s.replace(s.charAt(0), Character.toLowerCase(s.charAt(0)));
+			s = s.replaceFirst("^[A-Z]", String.valueOf(Character.toLowerCase(s.charAt(0))));
 		
-		return s;		
+		return s;
 	}
 
 	public static String upperFirstLetter(String s)
 	{
 		if(s != null && s.length() > 0)
-			s = s.replace(s.charAt(0), Character.toUpperCase(s.charAt(0)));
+			s = s.replaceFirst("^[a-z]", String.valueOf(Character.toUpperCase(s.charAt(0))));
 		
 		return s;		
 	}
