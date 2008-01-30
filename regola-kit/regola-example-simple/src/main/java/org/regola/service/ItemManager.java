9c5e9b8cc11294e28458f811939544feea3a3746
l.bragaglia <l.bragaglia@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2008-01-30 09:48:20 +0000

    Merged 1.0-RC2

---
+++ regola-kit/regola-example-simple/src/main/java/org/regola/service/ItemManager.java
@@ -0,0 +1,13 @@
+package org.regola.service;
+
+import org.regola.service.GenericManager;
+
+import org.regola.dao.GenericDao;
+import org.regola.model.Invoice;
+import org.regola.model.Item;
+import org.regola.model.ItemId;
+
+public interface ItemManager extends GenericManager<Item,ItemId>
+{
+
+}
---
+++ regola-kit/regola-example-simple/src/main/java/org/regola/service/MemoryItemManager.java
@@ -0,0 +1,9 @@
+package org.regola.service;
+
+import org.regola.model.Item;
+import org.regola.model.ItemId;
+
+public interface MemoryItemManager extends MemoryGenericManager<Item,ItemId>
+{
+
+}
