d4dc4a64494924c44c4a659da402c92f85abd7c4
fabio.cognigni <fabio.cognigni@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2008-07-23 15:12:44 +0000

    aggiunte funzionalità per manipolazione custom delle query HQL (per problema DISTINCT con CLOB)

--- trunk/regola-kit/regola-dao/regola-hibernate/src/main/java/org/regola/filter/criteria/hibernate/HibernateQueryBuilder.java
+++ trunk/regola-kit/regola-dao/regola-hibernate/src/main/java/org/regola/filter/criteria/hibernate/HibernateQueryBuilder.java
@@ -2,6 +2,7 @@ package org.regola.filter.criteria.hibernate;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.regola.filter.criteria.hibernate.support.HQLquery;
 import org.regola.filter.criteria.impl.BaseQueryBuilder;
 
 public class HibernateQueryBuilder extends BaseQueryBuilder {
@@ -33,4 +34,36 @@ public class HibernateQueryBuilder extends BaseQueryBuilder {
 	protected String getRootEntityCountAlias() {
 		return "*";
 	}
+	
+	public HQLquery getClausoleHQL() {
+		HQLquery hqlQuery = new HQLquery();
+		
+		hqlQuery.setParameters(getParameters());
+		hqlQuery.setFirstResult(getFirstResult());
+		hqlQuery.setMaxResult(getMaxResults());
+		hqlQuery.setRootEntity(getRootEntity());
+		
+		String queryHqlString = buildQuery();
+		
+		String[] pezzi = queryHqlString.split("from");
+		hqlQuery.setSelect(pezzi[0].replace("select", "").replace("distinct", ""));
+		String resto = pezzi[1];
+		if(resto.contains("where"))
+		{
+			pezzi = resto.split("where");
+			hqlQuery.setFrom(pezzi[0]);
+			resto = pezzi[1];
+		}
+		if(resto.contains("order by"))
+		{
+			pezzi = resto.split("order by");
+			hqlQuery.setOrderBy(pezzi[1]);
+		}
+		if(hqlQuery.getFrom() == null)
+			hqlQuery.setFrom(pezzi[0]);
+		else
+			hqlQuery.setWhere(pezzi[0]);
+		
+		return hqlQuery;
+	}	
 }
