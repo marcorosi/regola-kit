85938423a9653f39609d8ff91c32784193b7bc67
fabio.cognigni <fabio.cognigni@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2008-07-30 10:31:38 +0000

    pulizia codice

--- branches/1_0/regola-kit/regola-example-simple/src/main/java/org/regola/dao/hibernate/CustomerDaoHibernate.java
+++ branches/1_0/regola-kit/regola-example-simple/src/main/java/org/regola/dao/hibernate/CustomerDaoHibernate.java
@@ -2,8 +2,6 @@ package org.regola.dao.hibernate;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.criterion.CriteriaSpecification;
 import org.regola.dao.CustomerDao;
 import org.regola.model.Customer;
 
@@ -18,7 +16,7 @@ public class CustomerDaoHibernate extends HibernateGenericDao<Customer,Integer> 
 	{
 		
 		/*
-		 * Questa va in eccezione: controllare come poter mettere più di una colonna per la IN
+		 * Questa va in eccezione: hypersonic non gestisce più di una colonna nella in
 		 * 
 		String hql = "from Customer c1 where (c1.id,c1.lastName) in" +
 						"(select distinct(c2.id,c2.lastName) from Customer c2 where c2.id > 3)";
