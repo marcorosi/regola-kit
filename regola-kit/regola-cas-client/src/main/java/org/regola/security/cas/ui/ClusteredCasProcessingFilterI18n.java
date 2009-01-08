03933773ec97c52fe407283ffae80f7ec40d1652
marco.rosi <marco.rosi@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2009-01-08 10:15:27 +0000

    security fix

--- regola-kit/regola-cas-client/src/main/java/org/regola/security/cas/ui/ClusteredCasProcessingFilterI18n.java
+++ regola-kit/regola-cas-client/src/main/java/org/regola/security/cas/ui/ClusteredCasProcessingFilterI18n.java
@@ -1,9 +1,13 @@
 package org.regola.security.cas.ui;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.acegisecurity.Authentication;
 import org.acegisecurity.AuthenticationException;
@@ -15,98 +19,150 @@ import org.regola.security.cas.util.ClusteredUtils;
 import org.regola.security.cas.util.SessionIdExtractor;
 
 /**
- * Processa un CAS service ticket con il supporto per il funzionamento in cluster
- * con sticky session. 
- * Vedere {@link org.acegisecurity.ui.cas.CasProcessingFilter}
- * Modificato per aggiungere anche il parametro della lingua passato da CAS.
+ * Processa un CAS service ticket con il supporto per il funzionamento in
+ * cluster con sticky session. Vedere
+ * {@link org.acegisecurity.ui.cas.CasProcessingFilter} Modificato per
+ * aggiungere anche il parametro della lingua passato da CAS.
  * 
  * @author marco
- *
+ * 
  */
 public class ClusteredCasProcessingFilterI18n extends CasProcessingFilter {
 
-    private String languageParameter = "siteLanguage";
+	private String languageParameter = "siteLanguage";
 
-    private SessionIdExtractor sessionIdExtractor;
-    
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request)
-    	throws AuthenticationException 
-    {
-	    String username = CAS_STATEFUL_IDENTIFIER;
-	    String password = request.getParameter("ticket");
-	    String sessionId = sessionIdExtractor.getSessionIdFromCookie(request);
-	    
-	    if (password == null) {
-	        password = "";
-	    }
+	private SessionIdExtractor sessionIdExtractor;
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request)
+			throws AuthenticationException {
+		String username = CAS_STATEFUL_IDENTIFIER;
+		String password = request.getParameter("ticket");
+		String sessionId = sessionIdExtractor.getSessionIdFromCookie(request);
+
+		if (password == null) {
+			password = "";
+		}
+
+		if (sessionId == null)
+			throw new RuntimeException(
+					"Impossibile ricavare il session id dalla richiesta. Il funzionamento in cluster non è possibile.");
+
+		UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+				username, ClusteredUtils.encodePassword(password, sessionId));
+
+		authRequest.setDetails(authenticationDetailsSource
+				.buildDetails((HttpServletRequest) request));
+
+		return this.getAuthenticationManager().authenticate(authRequest);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request,
+			HttpServletResponse response, Authentication authResult)
+			throws IOException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Authentication success: " + authResult.toString());
+		}
+
+		SecurityContextHolder.getContext().setAuthentication(authResult);
+
+		if (logger.isDebugEnabled()) {
+			logger
+					.debug("Updated SecurityContextHolder to contain the following Authentication: '"
+							+ authResult + "'");
+		}
+
+		// Don't attempt to obtain the url from the saved request if
+		// alwaysUsedefaultTargetUrl is set
+		String targetUrl = isAlwaysUseDefaultTargetUrl() ? null
+				: obtainFullRequestUrl(request);
+
+		if (targetUrl == null) {
+			targetUrl = getDefaultTargetUrl();
+		}
+
+		// parametro della lingua
+		targetUrl = aggiungiParametroLingua(targetUrl, request);
+
+		if (logger.isDebugEnabled()) {
+			logger
+					.debug("Redirecting to target URL from HTTP Session (or default): "
+							+ targetUrl);
+		}
+
+		onSuccessfulAuthentication(request, response, authResult);
+
+		getRememberMeServices().loginSuccess(request, response, authResult);
+
+		// Fire event
+		if (this.eventPublisher != null) {
+			eventPublisher
+					.publishEvent(new InteractiveAuthenticationSuccessEvent(
+							authResult, this.getClass()));
+		}
+
+		sendRedirect(request, response, targetUrl);
+	}
+
+	/**
+	 * per motivi di sicurezza invalida la sessione creata prima del login 
+	 */
+	@Override
+	protected void onSuccessfulAuthentication(HttpServletRequest request,
+			HttpServletResponse response, Authentication authResult)
+			throws IOException {
+		HttpSession oldSession = request.getSession(false);
+
+		HashMap<String, Object> tmp = new HashMap<String, Object>();
+
+		if (oldSession != null) {
+
+			Enumeration enumer = oldSession.getAttributeNames();
+
+			while (enumer.hasMoreElements()) {
+
+				String s = (String) enumer.nextElement();
+
+				tmp.put(s, oldSession.getAttribute(s));
+
+			}
+
+			logger.debug("Sessione " + oldSession.getId() + " valida? "
+					+ request.isRequestedSessionIdValid());
+			oldSession.invalidate();
+
+		}
+
+		HttpSession newSession = request.getSession(true);
+
+		logger.debug("E adesso sessione " + newSession.getId() + " valida? "
+				+ request.isRequestedSessionIdValid());
+
+		for (Map.Entry<String, Object> entry : tmp.entrySet()) {
+
+			newSession.setAttribute(entry.getKey(), entry.getValue());
+
+		}
+
+		super.onSuccessfulAuthentication(request, response, authResult);
+	}
 	
-	    if(sessionId == null)
-	    	throw new RuntimeException("Impossibile ricavare il session id dalla richiesta. Il funzionamento in cluster non è possibile.");
-	    
-	    UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, ClusteredUtils.encodePassword(password,sessionId));
-	
-	    authRequest.setDetails(authenticationDetailsSource.buildDetails((HttpServletRequest) request));
-	
-	    return this.getAuthenticationManager().authenticate(authRequest);
-    }
-    
-    @Override
-	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-            Authentication authResult) throws IOException {
-            if (logger.isDebugEnabled()) {
-                logger.debug("Authentication success: " + authResult.toString());
-            }
-
-            SecurityContextHolder.getContext().setAuthentication(authResult);
-
-            if (logger.isDebugEnabled()) {
-                logger.debug("Updated SecurityContextHolder to contain the following Authentication: '" + authResult + "'");
-            }
-
-            // Don't attempt to obtain the url from the saved request if alwaysUsedefaultTargetUrl is set
-            String targetUrl = isAlwaysUseDefaultTargetUrl() ? null : obtainFullRequestUrl(request);
-
-            if (targetUrl == null) {
-                targetUrl = getDefaultTargetUrl();
-            }
-
-            //parametro della lingua
-           	targetUrl = aggiungiParametroLingua(targetUrl,request);
-            
-            if (logger.isDebugEnabled()) {
-                logger.debug("Redirecting to target URL from HTTP Session (or default): " + targetUrl);
-            }
-
-            onSuccessfulAuthentication(request, response, authResult);
-
-            getRememberMeServices().loginSuccess(request, response, authResult);
-
-            // Fire event
-            if (this.eventPublisher != null) {
-                eventPublisher.publishEvent(new InteractiveAuthenticationSuccessEvent(authResult, this.getClass()));
-            }
-
-            sendRedirect(request, response, targetUrl);
-        }
-
-	private String aggiungiParametroLingua(String targetUrl, HttpServletRequest request) 
-	{
+	private String aggiungiParametroLingua(String targetUrl,
+			HttpServletRequest request) {
 		String language = request.getParameter(getLanguageParameter());
-		
-		if(language==null || language.trim().length()==0)
+
+		if (language == null || language.trim().length() == 0)
 			return targetUrl;
-        
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(targetUrl);
-        if(targetUrl.indexOf("?") > 0)
-        	sb.append("&");
-        else
-        	sb.append("?");
-		sb.append(getLanguageParameter())
-			.append("=")
-			.append(language);
-		
+		if (targetUrl.indexOf("?") > 0)
+			sb.append("&");
+		else
+			sb.append("?");
+		sb.append(getLanguageParameter()).append("=").append(language);
+
 		return sb.toString();
 	}
 
@@ -115,7 +171,8 @@ public class ClusteredCasProcessingFilterI18n extends CasProcessingFilter {
 	}
 
 	/**
-	 * Imposta il nome del parametro da passare nella URL 
+	 * Imposta il nome del parametro da passare nella URL
+	 * 
 	 * @param languageParameter
 	 */
 	public void setLanguageParameter(String languageParameter) {
