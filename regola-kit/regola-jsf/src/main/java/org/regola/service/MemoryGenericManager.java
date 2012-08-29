150c9e44709804c79a28a5283941ddb9acef1d6a
l.bragaglia@gmail.com <l.bragaglia@gmail.com@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2012-08-29 09:59:51 +0000

    rimossi moduli obsoleti

--- regola-kit/regola-jsf/src/main/java/org/regola/service/MemoryGenericManager.java
+++
@@ -1,9 +0,0 @@
-package org.regola.service;
-
-import java.io.Serializable;
-import java.util.Collection;
-
-public interface MemoryGenericManager<T, PK extends Serializable> extends GenericManager<T, PK> 
-{
-	void setTarget(Collection<T> target);
-}
