355ae385e61489c73fd2a1781190394d415fcf1f
marco.rosi@gmail.com <marco.rosi@gmail.com@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2013-03-21 16:09:10 +0000

    passaggio a nuove API

--- trunk/regola-kit/regola-dao/regola-hibernate/src/main/java/org/regola/hibernate/type/SiONo.java
+++ trunk/regola-kit/regola-dao/regola-hibernate/src/main/java/org/regola/hibernate/type/SiONo.java
@@ -1,14 +1,51 @@
 package org.regola.hibernate.type;
 
-import org.hibernate.type.CharBooleanType;
+import java.io.Serializable;
 
-@SuppressWarnings("deprecation")
-public class SiONo extends CharBooleanType {
+import org.hibernate.dialect.Dialect;
+import org.hibernate.type.AbstractSingleColumnStandardBasicType;
+import org.hibernate.type.DiscriminatorType;
+import org.hibernate.type.PrimitiveType;
+import org.hibernate.type.StringType;
+import org.hibernate.type.descriptor.java.BooleanTypeDescriptor;
+import org.hibernate.type.descriptor.sql.CharTypeDescriptor;
 
-	private static final long serialVersionUID = 5095948002689497550L;
+/**
+ * tipo per mappare una colonna char contenente "S"/"N" in boolean
+ * 
+ * @author marco
+ *
+ */
+public class SiONo extends AbstractSingleColumnStandardBasicType<Boolean>
+implements PrimitiveType<Boolean>, DiscriminatorType<Boolean> {
+	
+	private static final long serialVersionUID = -3472703870522040453L;
+	
+	public static final SiONo INSTANCE = new SiONo();
 
 	public SiONo() {
-		super('S', 'N');
+		super( CharTypeDescriptor.INSTANCE, BooleanTypeDescriptor.INSTANCE );
+	}
+
+	public String getName() {
+		return "si_no";
+	}
+
+	@SuppressWarnings("rawtypes")
+	public Class getPrimitiveClass() {
+		return boolean.class;
+	}
+
+	public Boolean stringToObject(String xml) throws Exception {
+		return fromString( xml );
+	}
+
+	public Serializable getDefaultValue() {
+		return Boolean.FALSE;
+	}
+
+	public String objectToSQLString(Boolean value, Dialect dialect) throws Exception {
+		return StringType.INSTANCE.objectToSQLString( value.booleanValue() ? "S" : "N", dialect );
 	}
 
 }
