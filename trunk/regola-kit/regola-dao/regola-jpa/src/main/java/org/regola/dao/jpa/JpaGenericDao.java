e7802bf2f869c2c915ea9aba1ee939a575811254
nicolasanti@gmail.com <nicolasanti@gmail.com@5ec4ca14-563a-0410-b726-7bda155b1cd8>
2013-04-15 13:03:50 +0000

    Aggiunto il GenericDao2 ed UniversalDao2 (o meglio UniversalDuo!)

--- trunk/regola-kit/regola-dao/regola-jpa/src/main/java/org/regola/dao/jpa/JpaGenericDao.java
+++ trunk/regola-kit/regola-dao/regola-jpa/src/main/java/org/regola/dao/jpa/JpaGenericDao.java
@@ -6,15 +6,24 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
+import org.regola.dao.GenericDao2;
 import org.regola.dao.impl.AbstractGenericDao;
 import org.regola.filter.criteria.jpa.JpaQueryBuilder;
 import org.regola.model.ModelPattern;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 
+ * @author nicola
+ *
+ * @param <T>
+ * @param <ID>
+ */
 @Repository
 public class JpaGenericDao<T, ID extends Serializable> extends
-		AbstractGenericDao<T, ID> {
+		AbstractGenericDao<T, ID> implements GenericDao2<T,ID> {
 
 	protected EntityManager entityManager;
 
@@ -66,6 +75,53 @@ public class JpaGenericDao<T, ID extends Serializable> extends
 				getPersistentClass(), entityManager);
 		getPatternParser().createQuery(criteriaBuilder, filter);
 		return criteriaBuilder.getQuery().getResultList();
+	}
+
+	public List<T> queryList(String jpql, Object... params) {
+		TypedQuery<T> query =  this.entityManager.createQuery(jpql, getPersistentClass());
+	      int index = 0;
+	      for (Object param : params) query.setParameter(++index, param);
+	      return query.getResultList();
+	}
+
+	public List<T> queryListPaged(String jpql, int first, int max,
+			Object... params) {
+		 TypedQuery<T> query =  this.entityManager.createQuery(jpql, getPersistentClass());
+	        int index = 0;
+	        for (Object param : params) query.setParameter(++index, param);
+	        query.setFirstResult(first);
+	        query.setMaxResults(max);
+	        return query.getResultList();
+	}
+
+	public T querySingle(String jpql, Object... params) {
+		TypedQuery<T> query =  this.entityManager.createQuery(jpql, getPersistentClass());
+        int index = 0;
+        for (Object param : params) query.setParameter(++index, param);
+        return query.getSingleResult();
+	}
+
+	public int queryUpdate(String jpql, Object... params) {
+		Query query =  this.entityManager.createQuery(jpql);
+        int index = 0;
+        for (Object param : params) query.setParameter(++index, param);
+        return query.executeUpdate();
+	}
+
+	public void flush() {
+		 this.entityManager.flush();
+		
+	}
+
+	public void clear() {
+		this.entityManager.clear();
+		
+	}
+
+	public T merge(T entity) {
+		T merged = this.entityManager.merge(entity);
+	    this.entityManager.flush();
+	    return merged;
 	}
 
 }
